909472a28d1a7aea62fac8842c69ef8f4825f5ea
Александр Сенченко <senchenko.sashuta@list.ru>
2026-03-04 14:09:24 +0800

    переделано

--- Lab1.2/Program.cs
+++ Lab1.2/Program.cs
@@ -2,23 +2,30 @@
 {
     public class SignChangeLogic
     {
-        public static int CountSignChange(int[] massiv)
+        public static int countSignChange(int[] numbers)
         {
             var counter = 0;
 
-            if (massiv == null || massiv.Length < 2)
+            if (numbers == null || numbers.Length < 2)
             {
                 return 0;
             }
 
-            for (int i = 1; i < massiv.Length; i++)
+            for (int i = 1; i < numbers.Length; i++)
             {
-                if (massiv[i] * massiv[i - 1] < 0)
+                if (numbers[i] * numbers[i - 1] < 0)
                 {
                     counter++;
                 }
             }
             return counter;
+        }
+
+        public static string outputChangedSymbolCount(int[] numbers)
+        {
+            var counter = SignChangeLogic.countSignChange(numbers);
+            var result = "Знак меняется ровно: " + counter + " раз(а)";
+            return result;
         }
     }
     class Program
@@ -27,18 +34,16 @@
         {
             Console.WriteLine("Введите количество чисел в последовательности: ");
             var count = int.Parse(Console.ReadLine());
-            int[] massiv = new int[count];
+            int[] numbers = new int[count];
     
             for(int i = 0; i < count; i++)
             {
                 Console.WriteLine("Введите число в массив: ");
-                massiv[i] = int.Parse(Console.ReadLine());
+                numbers[i] = int.Parse(Console.ReadLine());
             }
 
-            var counter = SignChangeLogic.CountSignChange(massiv);
-            var result = "";
+            var result = SignChangeLogic.outputChangedSymbolCount(numbers);
 
-            result ="Знак меняется ровно: " + counter + " раз(а)";
             Console.WriteLine(result);
             }
     }
